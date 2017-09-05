df99f44bc638720235bd12e233955d96d606314e
Tom Gross <itconsense@gmail.com>
2017-09-05 15:32:22 +0200

    We need to specify the full schema for our behavior to avoid side effects

--- src/collective/sortedlisting/behavior.py
+++ src/collective/sortedlisting/behavior.py
@@ -2,7 +2,6 @@
 from collective.sortedlisting import _
 from collective.sortedlisting.widget import SortableQueryStringFieldWidget
 from plone.app.contenttypes.behaviors.collection import Collection
-from plone.app.contenttypes.behaviors.collection import ICollection
 from plone.autoform import directives as form
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.batching import Batch
@@ -13,11 +12,65 @@ from zope.interface import implementer_only
 from zope.interface import provider
 
 
-@provider(IFormFieldProvider)
-class ISortableCollectionBehavior(ICollection):
+from plone.supermodel import model
 
+# we need to inherit from model.Schema here directly
+# otherwise this gives some weird overlapping with the
+# ICollection interface
+@provider(IFormFieldProvider)
+class ISortableCollectionBehavior(model.Schema):
+
+    query = schema.List(
+        title=_(u'Search terms'),
+        description=_(u'Define the search terms for the items you want '
+                      u'to list by choosing what to match on. '
+                      u'The list of results will be dynamically updated'),
+        value_type=schema.Dict(value_type=schema.Field(),
+                               key_type=schema.TextLine()),
+        required=False,
+        missing_value=''
+    )
     # override QueryString widget with our sortable version
     form.widget('query', SortableQueryStringFieldWidget)
+
+    sort_on = schema.TextLine(
+        title=_(u'label_sort_on', default=u'Sort on'),
+        description=_(u'Sort the collection on this index'),
+        required=False,
+    )
+
+    sort_reversed = schema.Bool(
+        title=_(u'label_sort_reversed', default=u'Reversed order'),
+        description=_(u'Sort the results in reversed order'),
+        required=False,
+    )
+
+    limit = schema.Int(
+        title=_(u'Limit'),
+        description=_(u'Limit Search Results'),
+        required=False,
+        default=1000,
+        min=1,
+    )
+
+    item_count = schema.Int(
+        title=_(u'label_item_count', default=u'Item count'),
+        description=_(u'Number of items that will show up in one batch.'),
+        required=False,
+        default=30,
+        min=1,
+    )
+
+    customViewFields = schema.List(
+        title=_(u'Table Columns'),
+        description=_(u"Select which fields to display when "
+                      u"'Tabular view' is selected in the display menu."),
+        default=['Title', 'Creator', 'Type', 'ModificationDate'],
+        value_type=schema.Choice(
+            vocabulary='plone.app.contenttypes.metadatafields'),
+        required=False,
+    )
+
 
     # we need an additional field to store the sorting
     # as UIDs of the found objects (brains)
